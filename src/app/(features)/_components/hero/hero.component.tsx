8a1c74cdcb3ff911a9fba6fb68ae572e81855096
mubarekmuhiyie <mmexploit1@gmail.com>
2024-07-11 12:02:49 +0300

    fix comments

--- src/app/(features)/_components/hero/hero.component.tsx
+++ src/app/(features)/_components/hero/hero.component.tsx
@@ -1,8 +1,8 @@
 "use client";
-import { Box, Button, Stack, Text, useMantineTheme } from "@mantine/core";
+import { Box, Button, Flex, Stack, Text, useMantineTheme } from "@mantine/core";
 import React from "react";
 import HeroCar from "./hero-car.jpg";
-import Wave from "../services/Vector.svg"
+import Wave from "../services/Vector.svg";
 
 export default function HeroSection() {
   const theme = useMantineTheme();
@@ -25,31 +25,36 @@ export default function HeroSection() {
         }}
       >
         <Box className="w-full h-[100dvh] bg-black bg-opacity-35 top-0 relative z-3">
-          <Box className="flex min-h-full justify-center items-center ">
-            <Stack className="w-3/5 gap-6">
-              <Text
-                tt="uppercase"
-                className="text-white text-xl md:text-6xl text-center"
-                fw={"bold"}
-              >
-                Discover the World of Ultimate Comfort and Style
-              </Text>
+          <Stack className="w-3/5 justify-between h-full mx-auto">
+            <Text
+              tt="uppercase"
+              className="text-white text-xl md:text-6xl text-center mt-24"
+              fw={"bold"}
+            >
+              Discover the World of Ultimate Comfort and Style
+            </Text>
+            <Flex className="mb-8 items-center flex-col gap-4">
               <Text
                 tt="uppercase"
                 className="text-white text-xs md:text-xl text-center"
-              
               >
                 Embark on unforgettable journeys where luxury and elegance
                 accompany every step of your adventure.
               </Text>
+              
               <Button
                 color={theme.colors.secondary[9]}
-                className="w-fit mx-auto"
+                className="md:absolute md:right-2"
               >
-                <a href="https://book.mylimobiz.com/v4/uspholdings" target="_blank">Get Instant Quote</a>
+                <a
+                  href="https://book.mylimobiz.com/v4/uspholdings"
+                  target="_blank"
+                >
+                  Get Instant Quote
+                </a>
               </Button>
-            </Stack>
-          </Box>
+            </Flex>
+          </Stack>
         </Box>
       </Box>
     </Box>
