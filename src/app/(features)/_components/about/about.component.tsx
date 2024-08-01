ae7cf50da5f66b71108cc9de81f64d9fe8cb7f0d
Dagem Tsehay <84669128+dagemtsehay1@users.noreply.github.com>
2024-08-01 20:15:22 +0300

    feat(tendering): documentary evidence

--- src/app/(features)/_components/about/about.component.tsx
+++ src/app/(features)/_components/about/about.component.tsx
@@ -41,28 +41,28 @@ export default function About() {
           className="w-[15%] md:w-[5%] p-1"
         ></Box>
       </Flex>
-        <Stack className="md:w-2/5 pt-4 p-4 md:p-6 relative z-8">
-          {/* <Text className="text-justify font-semibold text-md md:text-lg text-white"> */}
-          <Text className="font-semibold text-md md:text-lg text-white">
-            In 2008, USP Holdings expanded its corporate mission and engaged in
-            shuttle bus and limousine services. Our transportation mission is to
-            provide safe, dependable, and efficient services for people who need
-            stress-free and economical travel. We have well-trained,
-            experienced, fully insured, professional, and courteous drivers. Our
-            vehicles, including shuttle buses, limousines, and taxi cabs, are
-            spread throughout the DMV and can be deployed immediately. Our
-            automobiles are well-maintained, and our staff is always punctual.
-            We provide services to a significant downtown corridor in
-            Washington, DC, Washington Dulles International, and Ronald Reagan
-            National Airports.
-          </Text>
+      <Stack className="md:w-2/5 pt-4 p-4 md:p-6 relative z-8">
+        {/* <Text className="text-justify font-semibold text-md md:text-lg text-white"> */}
+        <Text className="font-semibold text-md md:text-lg text-white">
+          In 2008, USP Holdings expanded its corporate mission and engaged in
+          transportation services. Our transportation mission is to provide
+          safe, dependable, and efficient services for people who need
+          stress-free and economical travel. We have well-trained, experienced,
+          fully insured, professional, and courteous drivers. Our vehicles
+          include Cadillacs, Mercedes Benz, Tesla, Range Rovers, Sprinters, and
+          coach buses that are spread throughout the DMV and can be deployed
+          immediately. Our automobiles are well-maintained, and our staff is
+          always punctual. We provide services to a significant downtown
+          corridor in Washington, DC, Washington Dulles International, and
+          Ronald Reagan National Airports.
+        </Text>
 
-          <Button color={theme.colors.secondary[9]} className="mr-auto">
-            <a href="https://book.mylimobiz.com/v4/uspholdings" target="_blank">
-              Book Now
-            </a>
-          </Button>
-        </Stack>
+        <Button color={theme.colors.secondary[9]} className="mr-auto">
+          <a href="https://book.mylimobiz.com/v4/uspholdings" target="_blank">
+            Book Now
+          </a>
+        </Button>
+      </Stack>
     </Stack>
     // </Box>
   );
