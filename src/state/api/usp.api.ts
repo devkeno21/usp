68eef71ed45e2d3b9a1b8d2c83698bc41808ae7a
Dagem Tsehay <84669128+dagemtsehay1@users.noreply.github.com>
2024-08-18 09:35:23 +0300

    url updated

--- src/state/api/usp.api.ts
+++ src/state/api/usp.api.ts
@@ -3,7 +3,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const uspApi = createApi({
   reducerPath: "uspApi",
   refetchOnFocus: true,
-  baseQuery: fetchBaseQuery({baseUrl:"http://31.220.31.112:4010/api/"}), // set This to  env variable
+  // baseQuery: fetchBaseQuery({baseUrl:"http://31.220.31.112:4010/api/"}), // set This to  env variable
+  baseQuery: fetchBaseQuery({baseUrl:"https://api-be.usptransportation.com/api/"}), // set This to  env variable
   // baseQuery: fetchBaseQuery({baseUrl:"https://qfrg768k-4000.uks1.devtunnels.ms/api/"}), // set This to  env variable
   endpoints: (builder) => ({
     joinAffiliate:builder.mutation<any,any>({
